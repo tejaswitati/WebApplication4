b6f08a7af58e19de24689368454df7139a3d917f
lalithyaduvvuru <lalithyaduvvuru@gmail.com>
2021-11-16 18:47:54 +0530

    upadeted lali

--- Startup.cs
+++ Startup.cs
@@ -33,6 +33,7 @@ namespace WebApplication4
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! lali");
                 });
             });
         }
